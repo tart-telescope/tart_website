b842535ef14c81e59d851c9b064245f34004e3c0
Max Scheel <max.scheel@cawthron.org.nz>
2022-10-12 23:15:13 +1300

    feat: must not overwrite the main branch with the static assets!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tart-telescope', // Usually your GitHub org/user name.
   projectName: 'tart_website', // Usually your repo name.
-  deploymentBranch: 'main', // Default is 'gh-pages' for non github.io 
+  deploymentBranch: 'gh-pages', // Default is 'gh-pages' for non github.io 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
