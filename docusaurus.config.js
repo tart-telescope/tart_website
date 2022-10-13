778f0e18f6cfc9424946b4175dabc1cd7430b3db
tim <tim@elec.ac.nz>
2022-10-13 14:29:49 +1300

    Not sure how this is going

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,10 +6,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'TART',
+  tagline: 'Transient Array Radio Telescope',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/tart_telescope/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
