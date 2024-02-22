1db72cce3b549479822cee4a6b885c012ac866ac
tim <tim@elec.ac.nz>
2024-02-22 17:11:13 +1300

    Rename to install elec

--- caddy/install.sh
+++
@@ -1,2 +0,0 @@
-rsync -rv --progress elec_site/ tart@tart.elec.ac.nz:caddy/elec_site
-rsync -rv --progress Caddyfile tart@tart.elec.ac.nz:caddy/Caddyfile
---
+++ caddy/install_elec.sh
@@ -0,0 +1,2 @@
+rsync -rv --progress elec_site/ tart@tart.elec.ac.nz:caddy/elec_site
+# rsync -rv --progress Caddyfile tart@tart.elec.ac.nz:caddy/Caddyfile
